ecf20026419b09ee0043756c82766c8e1a0571a7
GuyErreich <guyerreich@gmail.com>
2022-01-16 23:48:06 +0200

    Added animator to player

--- Assets/MyGame/Scripts/AnimationController.cs
+++ Assets/MyGame/Scripts/AnimationController.cs
@@ -9,18 +9,20 @@ public class AnimationController : MonoBehaviour
     private bool sprintPressed;
     private Vector2 movementInput;
     private Animator anim;
-    private int movementSpeedHash;
+    private int movementSpeedHash, rotationSpeedHash;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         movementSpeedHash = Animator.StringToHash("MovementSpeed");
+        rotationSpeedHash = Animator.StringToHash("Rotation");
     }
 
     // Update is called once per frame
     void Update()
     {
         anim.SetFloat(movementSpeedHash, movementInput.y);
+        anim.SetFloat(rotationSpeedHash, movementInput.x);
     }
 
     public void ReceiveInput(Vector2 _movementInput, bool _sprintPressed) {
